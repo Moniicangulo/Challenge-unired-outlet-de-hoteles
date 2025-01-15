37e30e0fab66d645842685e74af0218446ba7de8
Monica <angulosmonica@gmail.com>
2025-01-15 01:02:31 -0700

    fix api response

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@ const API_URL = "https://api-dev.outletdehoteles.com/api/availability/public";
 export async function getHotels(){
     try {
         const response = await axios.get(API_URL);
-        return response.data
+        return response.data.data
     } catch (error) {
         console.error("Error fetching hotels", error);
         throw new Error("Unable to fetch hotels.");
